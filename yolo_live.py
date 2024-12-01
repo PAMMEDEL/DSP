9c8e336b48db4ca6cda7ebc49c9dcb250a76df5b
Pamela Amor Medel <pamelaamorvm@gmail.com>
2024-12-01 16:16:16 +0800

    Update yolo_live.py

--- yolo_live.py
+++ yolo_live.py
@@ -51,7 +51,7 @@ def live_detection(plot_boxes, model_path="best.pt", webcam_resolution=(1280, 72
         if cv2.waitKey(1) & 0xFF == 27:
             break
 
-    cap.release()
+    .release()
 """
 import cv2
 import time
@@ -62,7 +62,7 @@ from draw_utils_live import plot_boxes_live, color_map_live
 # Function to handle live webcam detection
 def live_detection(plot_boxes, model_path="best.pt", webcam_resolution=(640, 480)):
     frame_width, frame_height = webcam_resolution
-    cap = cv2.VideoCapture(0)
+    cap = cv2.VideoCapture(2)
     cap.set(cv2.CAP_PROP_FRAME_WIDTH, frame_width)
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, frame_height)
     
